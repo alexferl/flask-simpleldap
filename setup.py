4fbd513f6a4c1e8de743ef65df6569a2b7accd28
Alexandre Ferland <3533424+alexferl@users.noreply.github.com>
2022-01-27 18:54:03 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,9 +29,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 )
